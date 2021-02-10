876ac9dd2c69263504a5d193cfe0b79a3f802315
nierax <axel.niering@lexasoft.de>
2021-02-10 14:08:59 +0100

    Use of Dice and DiceCup corrected

--- ls-mm-core/src/main/java/de/lexasoft/mastermind/core/MMStrategy.java
+++ ls-mm-core/src/main/java/de/lexasoft/mastermind/core/MMStrategy.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.lexasoft.common.model.Range;
 import de.lexasoft.game.Dice;
 import de.lexasoft.mastermind.core.api.MasterMindValidationException;
 import de.lexasoft.mastermind.core.api.NrOfColors;
@@ -22,94 +23,94 @@ import de.lexasoft.mastermind.core.api.QuestionPin;
  */
 public class MMStrategy {
 
-  private static Logger LOGGER = LoggerFactory.getLogger(MMStrategy.class);
+	private static Logger LOGGER = LoggerFactory.getLogger(MMStrategy.class);
 
-  private List<List<QuestionPin>> stillPossibleCombinations;
-  private NrOfColors nrOfColors;
-  private NrOfHoles nrOfHoles;
+	private List<List<QuestionPin>> stillPossibleCombinations;
+	private NrOfColors nrOfColors;
+	private NrOfHoles nrOfHoles;
 
-  /**
-   * 
-   */
-  public MMStrategy(NrOfColors nrOfColors, NrOfHoles nrOfHoles) {
-    stillPossibleCombinations = null;
-    this.nrOfColors = nrOfColors;
-    this.nrOfHoles = nrOfHoles;
-  }
+	/**
+	 * 
+	 */
+	public MMStrategy(NrOfColors nrOfColors, NrOfHoles nrOfHoles) {
+		stillPossibleCombinations = null;
+		this.nrOfColors = nrOfColors;
+		this.nrOfHoles = nrOfHoles;
+	}
 
-  List<List<QuestionPin>> getStillPossibleCombinations() {
-    return stillPossibleCombinations;
-  }
+	List<List<QuestionPin>> getStillPossibleCombinations() {
+		return stillPossibleCombinations;
+	}
 
-  void setStillPossibleCombinations(List<List<QuestionPin>> stillPossibleCombinations) {
-    this.stillPossibleCombinations = stillPossibleCombinations;
-  }
+	void setStillPossibleCombinations(List<List<QuestionPin>> stillPossibleCombinations) {
+		this.stillPossibleCombinations = stillPossibleCombinations;
+	}
 
-  private int indexOfNextGuess(List<List<QuestionPin>> leftCombinations) {
-    if (leftCombinations.size() == 1) {
-      return 0;
-    }
-    Dice dice = new Dice(1, leftCombinations.size());
-    return dice.roll() - 1;
-  }
+	private int indexOfNextGuess(List<List<QuestionPin>> leftCombinations) {
+		if (leftCombinations.size() == 1) {
+			return 0;
+		}
+		Dice dice = Dice.of(Range.of(1, leftCombinations.size()));
+		return dice.roll() - 1;
+	}
 
-  /**
-   * Gets the next guess. Needs the last guessed question and the answer to that
-   * question.
-   * 
-   * @param lastGuess  The guess made before.
-   * @param lastAnswer The answer to that guess.
-   * @return The next guess.
-   */
-  public QuestionBank nextGuess(QuestionBank lastGuess, AnswerBank lastAnswer) {
-    Long time = System.currentTimeMillis();
-    List<List<QuestionPin>> leftCombinations = new ArrayList<>();
-    Iterable<List<QuestionPin>> availableCombinations;
-    if (getStillPossibleCombinations() == null) {
-      availableCombinations = new CombinationCreator(nrOfColors, nrOfHoles);
-    } else {
-      availableCombinations = getStillPossibleCombinations();
-    }
-    QuestionBank toCheck = new QuestionBank(nrOfHoles, nrOfColors);
-    AnswerBank answer = new AnswerBank(nrOfHoles);
-    for (List<QuestionPin> combination2Check : availableCombinations) {
-      toCheck.doSetPins(combination2Check);
-      answer.removeAllPins();
-      answer = lastGuess.doAnswer(toCheck, answer);
-      if (lastAnswer.equals(answer)) {
-        leftCombinations.add(combination2Check);
-      }
-    }
-    if (leftCombinations.size() == 0) {
-      throw new MasterMindValidationException(
-          "There was a mistake in the answers, as no possible combinations remain.");
-    }
-    setStillPossibleCombinations(leftCombinations);
-    LOGGER.info(String.format("Left combinations: %s", getStillPossibleCombinations().size()));
-    QuestionBank nextGuess = new QuestionBank(nrOfHoles, nrOfColors);
-    nextGuess.setPins(leftCombinations.get(indexOfNextGuess(leftCombinations)));
-    LOGGER.info(String.format("Time used 2 guess: %sms", System.currentTimeMillis() - time));
-    return nextGuess;
-  }
+	/**
+	 * Gets the next guess. Needs the last guessed question and the answer to that
+	 * question.
+	 * 
+	 * @param lastGuess  The guess made before.
+	 * @param lastAnswer The answer to that guess.
+	 * @return The next guess.
+	 */
+	public QuestionBank nextGuess(QuestionBank lastGuess, AnswerBank lastAnswer) {
+		Long time = System.currentTimeMillis();
+		List<List<QuestionPin>> leftCombinations = new ArrayList<>();
+		Iterable<List<QuestionPin>> availableCombinations;
+		if (getStillPossibleCombinations() == null) {
+			availableCombinations = new CombinationCreator(nrOfColors, nrOfHoles);
+		} else {
+			availableCombinations = getStillPossibleCombinations();
+		}
+		QuestionBank toCheck = new QuestionBank(nrOfHoles, nrOfColors);
+		AnswerBank answer = new AnswerBank(nrOfHoles);
+		for (List<QuestionPin> combination2Check : availableCombinations) {
+			toCheck.doSetPins(combination2Check);
+			answer.removeAllPins();
+			answer = lastGuess.doAnswer(toCheck, answer);
+			if (lastAnswer.equals(answer)) {
+				leftCombinations.add(combination2Check);
+			}
+		}
+		if (leftCombinations.size() == 0) {
+			throw new MasterMindValidationException(
+			    "There was a mistake in the answers, as no possible combinations remain.");
+		}
+		setStillPossibleCombinations(leftCombinations);
+		LOGGER.info(String.format("Left combinations: %s", getStillPossibleCombinations().size()));
+		QuestionBank nextGuess = new QuestionBank(nrOfHoles, nrOfColors);
+		nextGuess.setPins(leftCombinations.get(indexOfNextGuess(leftCombinations)));
+		LOGGER.info(String.format("Time used 2 guess: %sms", System.currentTimeMillis() - time));
+		return nextGuess;
+	}
 
-  /**
-   * The first guess is just a randomly filled bank.
-   * 
-   * @return A randomly filled bank as a first guess.
-   */
-  public QuestionBank firstGuess() {
-    // Same logic as to create a solution. We reuse this method.
-    return createSolution();
-  }
+	/**
+	 * The first guess is just a randomly filled bank.
+	 * 
+	 * @return A randomly filled bank as a first guess.
+	 */
+	public QuestionBank firstGuess() {
+		// Same logic as to create a solution. We reuse this method.
+		return createSolution();
+	}
 
-  /**
-   * Creates a solution to be guessed by the player. The values are set randomly.
-   * 
-   * @return A solution to guess.
-   */
-  public QuestionBank createSolution() {
-    QuestionBank solution = new QuestionBank(nrOfHoles, nrOfColors);
-    return solution.roll();
-  }
+	/**
+	 * Creates a solution to be guessed by the player. The values are set randomly.
+	 * 
+	 * @return A solution to guess.
+	 */
+	public QuestionBank createSolution() {
+		QuestionBank solution = new QuestionBank(nrOfHoles, nrOfColors);
+		return solution.roll();
+	}
 
 }
--- ls-mm-core/src/main/java/de/lexasoft/mastermind/core/QuestionBank.java
+++ ls-mm-core/src/main/java/de/lexasoft/mastermind/core/QuestionBank.java
@@ -5,6 +5,7 @@ package de.lexasoft.mastermind.core;
 
 import java.util.List;
 
+import de.lexasoft.common.model.Range;
 import de.lexasoft.game.DiceCup;
 import de.lexasoft.mastermind.core.api.NrOfColors;
 import de.lexasoft.mastermind.core.api.NrOfHoles;
@@ -18,223 +19,223 @@ import de.lexasoft.mastermind.core.api.QuestionPin;
  */
 public class QuestionBank extends AnyBank<QuestionPin> {
 
-  private NrOfColors nrOfColors;
-  private DiceCup diceCup;
+	private NrOfColors nrOfColors;
+	private DiceCup diceCup;
 
-  /**
-   * Adds a counted attribute to the pin, which is needed for the internal
-   * answering process.
-   */
-  class InternalQuestionPin extends QuestionPin {
-    private boolean counted;
+	/**
+	 * Adds a counted attribute to the pin, which is needed for the internal
+	 * answering process.
+	 */
+	class InternalQuestionPin extends QuestionPin {
+		private boolean counted;
 
-    public InternalQuestionPin(QuestionPin pin) {
-      super(getNrOfColors(), pin.getColor());
-      setValue(pin.getColor());
-      counted = false;
-    }
+		public InternalQuestionPin(QuestionPin pin) {
+			super(getNrOfColors(), pin.getColor());
+			setValue(pin.getColor());
+			counted = false;
+		}
 
-    /**
-     * 
-     * @return True, if the pin has already been counted, false otherwise.
-     */
-    boolean isCounted() {
-      return counted;
-    }
+		/**
+		 * 
+		 * @return True, if the pin has already been counted, false otherwise.
+		 */
+		boolean isCounted() {
+			return counted;
+		}
 
-    /**
-     * Marks the pin counted.
-     */
-    void setCounted() {
-      counted = true;
-    }
+		/**
+		 * Marks the pin counted.
+		 */
+		void setCounted() {
+			counted = true;
+		}
 
-    /**
-     * Removes the counted mark from the pin.
-     */
-    void resetCounted() {
-      counted = false;
-    }
+		/**
+		 * Removes the counted mark from the pin.
+		 */
+		void resetCounted() {
+			counted = false;
+		}
 
-  }
+	}
 
-  /**
-   * Creates a bank of pins with the given number of pins, each with a range of
-   * the given number of colors.
-   * 
-   * @param nrOfHoles  Number of pins in the bank.
-   * @param nrOfColors Range of colors, every pin can represent.
-   */
-  public QuestionBank(NrOfHoles nrOfHoles, NrOfColors nrOfColors) {
-    super(nrOfHoles);
-    this.nrOfColors = nrOfColors;
-    diceCup = new DiceCup(getNrOfHoles().getValue(), 1, nrOfColors.getValue());
-  }
+	/**
+	 * Creates a bank of pins with the given number of pins, each with a range of
+	 * the given number of colors.
+	 * 
+	 * @param nrOfHoles  Number of pins in the bank.
+	 * @param nrOfColors Range of colors, every pin can represent.
+	 */
+	public QuestionBank(NrOfHoles nrOfHoles, NrOfColors nrOfColors) {
+		super(nrOfHoles);
+		this.nrOfColors = nrOfColors;
+		diceCup = DiceCup.of(getNrOfHoles().getValue(), Range.of(1, nrOfColors.getValue()));
+	}
 
-  NrOfColors getNrOfColors() {
-    return nrOfColors;
-  }
+	NrOfColors getNrOfColors() {
+		return nrOfColors;
+	}
 
-  /**
-   * Sets all pins counted.
-   */
-  void setAllPinsCounted() {
-    for (Hole<QuestionPin> hole : getHoles()) {
-      if (hole.holdsAPin()) {
-        ((InternalQuestionPin) hole.getPin()).setCounted();
-      }
-    }
-  }
+	/**
+	 * Sets all pins counted.
+	 */
+	void setAllPinsCounted() {
+		for (Hole<QuestionPin> hole : getHoles()) {
+			if (hole.holdsAPin()) {
+				((InternalQuestionPin) hole.getPin()).setCounted();
+			}
+		}
+	}
 
-  /**
-   * Reset all pins counted.
-   */
-  void resetAllPinsCounted() {
-    for (Hole<QuestionPin> hole : getHoles()) {
-      if (hole.holdsAPin()) {
-        ((InternalQuestionPin) hole.getPin()).resetCounted();
-      }
-    }
-  }
+	/**
+	 * Reset all pins counted.
+	 */
+	void resetAllPinsCounted() {
+		for (Hole<QuestionPin> hole : getHoles()) {
+			if (hole.holdsAPin()) {
+				((InternalQuestionPin) hole.getPin()).resetCounted();
+			}
+		}
+	}
 
-  /**
-   * @param solution The bank to compare with
-   * @return Number of black hits between these two banks.
-   */
-  private int countBlackHits(QuestionBank solution) {
-    int blackHits = 0;
-    for (int i = 0; i < getNrOfHoles().getValue(); i++) {
-      InternalQuestionPin myPin = (InternalQuestionPin) getPin(i);
-      InternalQuestionPin solutionPin = (InternalQuestionPin) solution.getPin(i);
-      // Hits at the same position
-      if (solutionPin.comparePin(myPin)) {
-        blackHits++;
-        solutionPin.setCounted();
-        myPin.setCounted();
-      }
-    }
-    return blackHits;
-  }
+	/**
+	 * @param solution The bank to compare with
+	 * @return Number of black hits between these two banks.
+	 */
+	private int countBlackHits(QuestionBank solution) {
+		int blackHits = 0;
+		for (int i = 0; i < getNrOfHoles().getValue(); i++) {
+			InternalQuestionPin myPin = (InternalQuestionPin) getPin(i);
+			InternalQuestionPin solutionPin = (InternalQuestionPin) solution.getPin(i);
+			// Hits at the same position
+			if (solutionPin.comparePin(myPin)) {
+				blackHits++;
+				solutionPin.setCounted();
+				myPin.setCounted();
+			}
+		}
+		return blackHits;
+	}
 
-  /**
-   * @param solution The bank to compare with
-   * @return Number of white hits between these two banks.
-   */
-  private int countWhiteHits(QuestionBank solution) {
-    int whiteHits = 0;
-    for (int i = 0; i < getNrOfHoles().getValue(); i++) {
-      InternalQuestionPin myPin = (InternalQuestionPin) getPin(i);
-      // If the pin had a black hit before, it must not be counted again.
-      if (!myPin.isCounted()) {
-        for (int j = 0; j < solution.getNrOfHoles().getValue(); j++) {
-          InternalQuestionPin solutionPin = (InternalQuestionPin) solution.getPin(j);
-          // Same index would be black and must not be counted
-          // If the solution pin was counted before, we must not count this one
-          if ((i != j) && !solutionPin.isCounted()) {
-            if (solutionPin.comparePin(myPin)) {
-              // One more hit
-              whiteHits++;
-              // Mark solution as counted
-              solutionPin.setCounted();
-              // Just count once. Because we break here, it is not necessary to set this pin
-              // as counted.
-              break;
-            }
-          }
-        }
-      }
-    }
-    return whiteHits;
-  }
+	/**
+	 * @param solution The bank to compare with
+	 * @return Number of white hits between these two banks.
+	 */
+	private int countWhiteHits(QuestionBank solution) {
+		int whiteHits = 0;
+		for (int i = 0; i < getNrOfHoles().getValue(); i++) {
+			InternalQuestionPin myPin = (InternalQuestionPin) getPin(i);
+			// If the pin had a black hit before, it must not be counted again.
+			if (!myPin.isCounted()) {
+				for (int j = 0; j < solution.getNrOfHoles().getValue(); j++) {
+					InternalQuestionPin solutionPin = (InternalQuestionPin) solution.getPin(j);
+					// Same index would be black and must not be counted
+					// If the solution pin was counted before, we must not count this one
+					if ((i != j) && !solutionPin.isCounted()) {
+						if (solutionPin.comparePin(myPin)) {
+							// One more hit
+							whiteHits++;
+							// Mark solution as counted
+							solutionPin.setCounted();
+							// Just count once. Because we break here, it is not necessary to set this pin
+							// as counted.
+							break;
+						}
+					}
+				}
+			}
+		}
+		return whiteHits;
+	}
 
-  /**
-   * Both the question and the solution must be filled completely.
-   * 
-   * @param question
-   * @param solution
-   */
-  private void validateBanks(QuestionBank question, QuestionBank solution) {
-    if (!question.isCompletelyFilled() || !solution.isCompletelyFilled()) {
-      throw new IllegalArgumentException("Both question and solution must be filled completely");
-    }
-    if (question.getNrOfHoles().getValue() != solution.getNrOfHoles().getValue()) {
-      throw new IllegalArgumentException("Both question and solution must have the same number of holes.");
-    }
-  }
+	/**
+	 * Both the question and the solution must be filled completely.
+	 * 
+	 * @param question
+	 * @param solution
+	 */
+	private void validateBanks(QuestionBank question, QuestionBank solution) {
+		if (!question.isCompletelyFilled() || !solution.isCompletelyFilled()) {
+			throw new IllegalArgumentException("Both question and solution must be filled completely");
+		}
+		if (question.getNrOfHoles().getValue() != solution.getNrOfHoles().getValue()) {
+			throw new IllegalArgumentException("Both question and solution must have the same number of holes.");
+		}
+	}
 
-  /**
-   * Compares this bank to the solution.
-   * 
-   * @param solution The bank with the solution, given to guess.
-   * @return The answer bank with the number of white and black pins
-   */
-  public AnswerBank answer(QuestionBank solution) {
-    validateBanks(this, solution);
-    return doAnswer(solution, new AnswerBank(getNrOfHoles()));
-  }
+	/**
+	 * Compares this bank to the solution.
+	 * 
+	 * @param solution The bank with the solution, given to guess.
+	 * @return The answer bank with the number of white and black pins
+	 */
+	public AnswerBank answer(QuestionBank solution) {
+		validateBanks(this, solution);
+		return doAnswer(solution, new AnswerBank(getNrOfHoles()));
+	}
 
-  /**
-   * Compares without checking, whether the banks are completely filled.
-   * <p>
-   * Can be used to save time, if the context makes sure, that the banks are
-   * filled.
-   * 
-   * @param solution The bank with the solution, given to guess.
-   * @param answer   The answer bank to use.
-   * @return The answer bank with the number of white and black pins
-   */
-  AnswerBank doAnswer(QuestionBank solution, AnswerBank answer) {
-    // Count black hits. Must be done first.
-    answer.addBlackPins(countBlackHits(solution));
-    // White hits second
-    answer.addWhitePins(countWhiteHits(solution));
-    // Mark answer as given.
-    answer.setGiven();
-    // Solution must be ready for the next answer.
-    solution.resetAllPinsCounted();
-    resetAllPinsCounted();
-    return answer;
-  }
+	/**
+	 * Compares without checking, whether the banks are completely filled.
+	 * <p>
+	 * Can be used to save time, if the context makes sure, that the banks are
+	 * filled.
+	 * 
+	 * @param solution The bank with the solution, given to guess.
+	 * @param answer   The answer bank to use.
+	 * @return The answer bank with the number of white and black pins
+	 */
+	AnswerBank doAnswer(QuestionBank solution, AnswerBank answer) {
+		// Count black hits. Must be done first.
+		answer.addBlackPins(countBlackHits(solution));
+		// White hits second
+		answer.addWhitePins(countWhiteHits(solution));
+		// Mark answer as given.
+		answer.setGiven();
+		// Solution must be ready for the next answer.
+		solution.resetAllPinsCounted();
+		resetAllPinsCounted();
+		return answer;
+	}
 
-  /**
-   * All pins are set with random values.
-   * <p>
-   * This happens for all pins, even if they have had a value before.
-   * 
-   * @return The question bank with the new values.
-   */
-  public QuestionBank roll() {
-    removeAllPins();
-    List<Integer> values = diceCup.roll();
-    for (Integer value : values) {
-      // Subtract 1 from the value, because dice is 1-based, but pins are 0-based.
-      addPin(new QuestionPin(nrOfColors, new PinColor(value - 1)));
-    }
-    return this;
-  }
+	/**
+	 * All pins are set with random values.
+	 * <p>
+	 * This happens for all pins, even if they have had a value before.
+	 * 
+	 * @return The question bank with the new values.
+	 */
+	public QuestionBank roll() {
+		removeAllPins();
+		List<Integer> values = diceCup.roll();
+		for (Integer value : values) {
+			// Subtract 1 from the value, because dice is 1-based, but pins are 0-based.
+			addPin(new QuestionPin(nrOfColors, new PinColor(value - 1)));
+		}
+		return this;
+	}
 
-  /**
-   * Replaces the pin by an instance of question pin.
-   * <p>
-   * This is necessary for being able to mark pins as counted under the answering
-   * process.
-   */
-  @Override
-  QuestionPin addPin(QuestionPin pin, int position) {
-    InternalQuestionPin myPin = new InternalQuestionPin((QuestionPin) pin);
-    return super.addPin(myPin, position);
-  }
+	/**
+	 * Replaces the pin by an instance of question pin.
+	 * <p>
+	 * This is necessary for being able to mark pins as counted under the answering
+	 * process.
+	 */
+	@Override
+	QuestionPin addPin(QuestionPin pin, int position) {
+		InternalQuestionPin myPin = new InternalQuestionPin((QuestionPin) pin);
+		return super.addPin(myPin, position);
+	}
 
-  /**
-   * Replaces the pin by an instance of question pin.
-   * <p>
-   * This is necessary for being able to mark pins as counted under the answering
-   * process.
-   */
-  @Override
-  QuestionPin addPin(QuestionPin pin) {
-    InternalQuestionPin myPin = new InternalQuestionPin((QuestionPin) pin);
-    return super.addPin(myPin);
-  }
+	/**
+	 * Replaces the pin by an instance of question pin.
+	 * <p>
+	 * This is necessary for being able to mark pins as counted under the answering
+	 * process.
+	 */
+	@Override
+	QuestionPin addPin(QuestionPin pin) {
+		InternalQuestionPin myPin = new InternalQuestionPin((QuestionPin) pin);
+		return super.addPin(myPin);
+	}
 
 }
